7b4b1d1546242db6de1f4713d4a6d3be5644be8f
deitydivan <83013819+deitydivan@users.noreply.github.com>
2022-05-24 20:35:02 +0300

    h

--- tgbot/config.py
+++ tgbot/config.py
@@ -48,4 +48,4 @@ def load_config(path: str = None):
         ),
         misc=Miscellaneous()
     )
-DB_URI = 'postgres://rlfdvcditosxia:550d193d8fb7962cb85178198e5f9612dbf11e12a08d2a0731adbfffb1943561@ec2-54-228-218-84.eu-west-1.compute.amazonaws.com:5432/d9s51kmjiegnth'
+DB_URI = 'postgres://bitvhbpezunplq:288ca835f6a2607eb69fa390987c930c97c72f3273dccb1b5631c4c9b8dc1db4@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d4a7bh5sl4dk3j'
